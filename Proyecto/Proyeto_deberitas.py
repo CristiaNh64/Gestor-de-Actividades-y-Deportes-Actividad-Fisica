2dea350fc4a4cdf0936e8ae4306f41bd3171df2a
CristiaNh64 <cristianhantisshoru@gmail.com>
2025-06-27 11:58:34 -0300

    Update Proyeto_deberitas.py

--- Proyecto/Proyeto_deberitas.py
+++ Proyecto/Proyeto_deberitas.py
@@ -1,14 +1,17 @@
 import tkinter as tk
+from tkinter import messagebox # Importamos messagebox para mostrar mensajes
 
-#Creamos la ventana que se vera durante todo el programa
+# Ventana que pricipal que se vera en el programa
 ventana_principal = tk.Tk()
 ventana_principal.geometry("600x600")
 ventana_principal.config(background="#121212")
 ventana_principal.title("Proyecto GYM")
 
-#Funcion utilizada para ir cambiando de ventanas
+# Variable para almacenar el nombre de usuario
+nombre_usuario = ""
+
+# Funcion utilizada para ir cambiando de ventanas
 def limpiar_ventana():
+    # Destruye todos los widgets hijos de la ventana principal
     for widget in ventana_principal.winfo_children():
         widget.destroy()
-
-ventana_principal.mainloop()
